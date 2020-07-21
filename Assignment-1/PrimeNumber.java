9f0cd6b6927b518bcd695c1a98d37b8aabfea616
Manju789 <68197488+Manju789@users.noreply.github.com>
2020-07-21 16:32:30 +0530

    Update PrimeNumber.java

--- Assignment-1/PrimeNumber.java
+++ Assignment-1/PrimeNumber.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner;
+public class PrimeNumber { //This class predicts whether the entered number is prime or not
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number ");
+		int n = sc.nextInt();
+		sc.close();
+		int temp=0;
+		for(int i=2;i<=n/2;i++) {
+			if(n%i==0) {
+				temp=1;
+				break;
+			}
+		}
+		if (temp==1)
+			System.out.println(n + " is not a prime number");
+		else
+			System.out.println(n + " is a prime number");
+	}
+}
